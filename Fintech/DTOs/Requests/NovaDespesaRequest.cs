e06438b26a695229cd570f45baa6662b1e111bde
Davi Freitas <davi.frrs@outlook.com>
2025-01-21 18:27:38 -0300

    Despesas REquest

---
+++ Fintech/DTOs/Requests/NovaDespesaRequest.cs
@@ -0,0 +1,12 @@
+﻿using Fintech.Enums;
+
+namespace Fintech.DTOs.Requests;
+
+public class NovaDespesaRequest
+{
+    public string Nome { get; set; } = string.Empty;
+    public string Origem { get; set; } = string.Empty;
+    public DateTime Data { get; set; }
+    public FormaDePagamento FormaDePagamento { get; set; }
+    public decimal Valor { get; set; }
+}
